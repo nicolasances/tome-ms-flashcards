8732d5ce1d9475cccde27a3d53c733b59637e266
nicolasances <nicolas.matteazzi@gmail.com>
2025-07-22 12:35:38 +0200

    PUt everything under try catch

--- src/cards/FlashcardsGenerator.ts
+++ src/cards/FlashcardsGenerator.ts
@@ -44,69 +44,69 @@ export class FlashcardsGenerator {
 
         this.logger.compute(this.cid, `Generating Flashcards for topic ${topicCode}`)
 
-        // 1. Retrieve from GCS all the files related to the specified topic (topic code)
-        // 1.1. Get the bucket
-        const storage = new Storage();
-        const bucket = storage.bucket(this.bucketName);
-
-        // 1.2 Get all the files in the folder {kbBaseFolder}/{topicCode}
-        const [files] = await bucket.getFiles({ prefix: `${this.kbBaseFolder}/${topicCode}/` });
-
-        this.logger.compute(this.cid, `Found ${files.length} files in knowledge base for topic ${topicCode}`)
-
-        // 2. For each file in the bucket read the content (text) and prompt an LLM to generate flashcards 
-        // Run in parallel and wait for all requests to be done
-        let promises = [];
-        for (const file of files) {
-
-            // 2.1. Read the file content
-            const [fileBuffer] = await file.download();
-            const fileContent = fileBuffer.toString('utf-8');
-
-            // Extract the section code from the file name (the file name is expected to be in the format {sectionCode}.txt)
-            const sectionCode = file.name.split('/').pop()?.replace('.txt', '');
-
-            // 2.1 Generate a timeline flashcard for each file
-            promises.push(async (): Promise<SectionTimelineFC[]> => {
-
-                this.logger.compute(this.cid, `Generating timeline flashcard for file ${file.name}`)
-
-                const timelineFlashcards = await new SectionTimelineFCGenerator(this.execContext, this.request, this.user, topicCode, topicId, sectionCode!).generateFlashcards(fileContent);
-
-                this.logger.compute(this.cid, `Generated timeline flashcard for file ${file.name}`)
-
-                return timelineFlashcards;
-            })
-
-            // 2.2 Generate multiple options flashcards for each file
-            promises.push(async (): Promise<MultipleOptionsFC[]> => {
-
-                this.logger.compute(this.cid, `Reading content of kb file ${file.name}`)
-
-                // 2.2. Create the flashcards with an LLM
-                this.logger.compute(this.cid, `Prompting LLM to generate section's flashcards for file ${file.name}`)
-
-                const generatedFlashcards = await new MultipleOptionsFCGenerator(this.execContext, this.request, this.user, topicCode, topicId, sectionCode!).generateFlashcards(fileContent);
-
-                this.logger.compute(this.cid, `LLM responded with ${generatedFlashcards.length} flashcards for file ${file.name}`)
-
-                return generatedFlashcards;
-
-            })
-        }
-
-        // 2.3. Wait for all the promises to finish 
-        const promisesResult = await Promise.all(promises.map(fn => fn()));
-
-        const generatedFlashcards = promisesResult.flat();
-
-        this.logger.compute(this.cid, `Generated ${generatedFlashcards.length} flashcards for topic ${topicCode}`);
-
-        // 3. Save all the generated flashcards
         let client;
 
         try {
 
+            // 1. Retrieve from GCS all the files related to the specified topic (topic code)
+            // 1.1. Get the bucket
+            const storage = new Storage();
+            const bucket = storage.bucket(this.bucketName);
+
+            // 1.2 Get all the files in the folder {kbBaseFolder}/{topicCode}
+            const [files] = await bucket.getFiles({ prefix: `${this.kbBaseFolder}/${topicCode}/` });
+
+            this.logger.compute(this.cid, `Found ${files.length} files in knowledge base for topic ${topicCode}`)
+
+            // 2. For each file in the bucket read the content (text) and prompt an LLM to generate flashcards 
+            // Run in parallel and wait for all requests to be done
+            let promises = [];
+            for (const file of files) {
+
+                // 2.1. Read the file content
+                const [fileBuffer] = await file.download();
+                const fileContent = fileBuffer.toString('utf-8');
+
+                // Extract the section code from the file name (the file name is expected to be in the format {sectionCode}.txt)
+                const sectionCode = file.name.split('/').pop()?.replace('.txt', '');
+
+                // 2.1 Generate a timeline flashcard for each file
+                promises.push(async (): Promise<SectionTimelineFC[]> => {
+
+                    this.logger.compute(this.cid, `Generating timeline flashcard for file ${file.name}`)
+
+                    const timelineFlashcards = await new SectionTimelineFCGenerator(this.execContext, this.request, this.user, topicCode, topicId, sectionCode!).generateFlashcards(fileContent);
+
+                    this.logger.compute(this.cid, `Generated timeline flashcard for file ${file.name}`)
+
+                    return timelineFlashcards;
+                })
+
+                // 2.2 Generate multiple options flashcards for each file
+                promises.push(async (): Promise<MultipleOptionsFC[]> => {
+
+                    this.logger.compute(this.cid, `Reading content of kb file ${file.name}`)
+
+                    // 2.2. Create the flashcards with an LLM
+                    this.logger.compute(this.cid, `Prompting LLM to generate section's flashcards for file ${file.name}`)
+
+                    const generatedFlashcards = await new MultipleOptionsFCGenerator(this.execContext, this.request, this.user, topicCode, topicId, sectionCode!).generateFlashcards(fileContent);
+
+                    this.logger.compute(this.cid, `LLM responded with ${generatedFlashcards.length} flashcards for file ${file.name}`)
+
+                    return generatedFlashcards;
+
+                })
+            }
+
+            // 2.3. Wait for all the promises to finish 
+            const promisesResult = await Promise.all(promises.map(fn => fn()));
+
+            const generatedFlashcards = promisesResult.flat();
+
+            this.logger.compute(this.cid, `Generated ${generatedFlashcards.length} flashcards for topic ${topicCode}`);
+
+            // 3. Save all the generated flashcards
             client = await this.config.getMongoClient();
             const db = client.db(this.config.getDBName());
 
@@ -119,6 +119,17 @@ export class FlashcardsGenerator {
             const insertedCount = await fcStore.saveFlashCards(generatedFlashcards);
 
             this.logger.compute(this.cid, `Persisted ${insertedCount} flashcards for topic ${topicCode}`);
+
+            // 5. Publish an event that flashcards have been generated for the topic
+            new EventPublisher(this.execContext, "tometopics").publishEvent(topicId, EVENTS.flashcardsCreated, `Flashcards generated for topic ${topicCode}`, new FlashcardsCreatedEvent(
+                getFlashcardsGeneration(),
+                topicCode,
+                topicId,
+                generatedFlashcards.length,
+            ))
+
+            // 5. Return the generated flashcards
+            return { flashcards: generatedFlashcards }
 
         } catch (error) {
 
@@ -136,16 +147,5 @@ export class FlashcardsGenerator {
         finally {
             if (client) client.close();
         }
-
-        // 5. Publish an event that flashcards have been generated for the topic
-        new EventPublisher(this.execContext, "tometopics").publishEvent(topicId, EVENTS.flashcardsCreated, `Flashcards generated for topic ${topicCode}`, new FlashcardsCreatedEvent(
-            getFlashcardsGeneration(), 
-            topicCode, 
-            topicId,
-            generatedFlashcards.length,
-        ))
-
-        // 5. Return the generated flashcards
-        return { flashcards: generatedFlashcards }
     }
 }
