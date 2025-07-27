a10bf6b2a541f7afbe30fa4d7716c953fe6e3fd6
nicolasances <nicolas.matteazzi@gmail.com>
2025-07-27 13:32:21 +0200

    Added date generation

--- src/util/FlashcardGeneration.ts
+++ src/util/FlashcardGeneration.ts
@@ -1,6 +1,7 @@
+import { DateFCGenerator } from "../cards/generators/DateFCGenerator";
 import { MultipleOptionsFCGenerator } from "../cards/generators/MultipleOptionsFCGenerator";
 import { SectionTimelineFCGenerator } from "../cards/generators/SectionTimelineFCGenerator";
 
 export function getFlashcardsGeneration() {
-    return `${SectionTimelineFCGenerator.generation()}-${MultipleOptionsFCGenerator.generation()}`;
+    return `${SectionTimelineFCGenerator.generation()}-${MultipleOptionsFCGenerator.generation()}-${DateFCGenerator.generation()}`;
 }
