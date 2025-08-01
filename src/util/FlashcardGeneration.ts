2d5117682270f9fc338b723d66ddabbe2bac2d88
nicolasances <nicolas.matteazzi@gmail.com>
2025-08-01 06:10:25 +0200

    Generation number of historical graph

--- src/util/FlashcardGeneration.ts
+++ src/util/FlashcardGeneration.ts
@@ -1,7 +1,7 @@
 import { DateFCGenerator } from "../cards/generators/DateFCGenerator";
+import { HistoricalGraphGenerator } from "../cards/generators/HistoricalGraphGenerator";
 import { MultipleOptionsFCGenerator } from "../cards/generators/MultipleOptionsFCGenerator";
-import { SectionTimelineFCGenerator } from "../cards/generators/SectionTimelineFCGenerator";
 
 export function getFlashcardsGeneration() {
-    return `${SectionTimelineFCGenerator.generation()}-${MultipleOptionsFCGenerator.generation()}-${DateFCGenerator.generation()}`;
+    return `${HistoricalGraphGenerator.generation()}-${MultipleOptionsFCGenerator.generation()}-${DateFCGenerator.generation()}`;
 }
